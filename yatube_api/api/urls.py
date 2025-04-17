58360700a5ce11cb591aa37609817916be366375
Danil Shubenkin <shubenkindana@gmail.com>
2025-04-18 04:29:51 +0500

    new

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,7 +5,7 @@ from .views import PostViewSet, GroupViewSet, CommentViewSet
 
 router = SimpleRouter()
 router.register("posts", PostViewSet, basename="posts")
-router.register("group", GroupViewSet, basename="group")
+router.register("groups", GroupViewSet, basename="groups")
 router.register(
     r"posts/(?P<post_id>\d+)/comments",
     CommentViewSet,
